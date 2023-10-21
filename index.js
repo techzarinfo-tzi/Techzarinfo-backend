03b5069fc664be8c3f63cfb307511c5b640b5782
easwartechzar <easwar.techzar@gmail.com>
2023-10-21 13:26:21 +0530

    cahgnes are done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(
 // }
 app.set("trust proxy", 1);
 app.use(cookieParser());
-app.use(cors({ credentials: true,  }));
+app.use(cors({ credentials: true, origin: "*" }));
 const path = require("path");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
